409e74371cc5e815825acd156ec6b4d842e54e34
activey <activey@doaplatform.org>
2025-11-03 16:14:45 +0100

    Added some content to main page with experimental carousel in hero section.

---
+++ app/components/HeroCarousel.vue
@@ -0,0 +1,29 @@
+<template>
+  <ClientOnly>
+    <Splide
+      :options="{
+        type: 'loop',
+        autoplay: true,
+        interval: 4000,
+        arrows: true,
+        pagination: false,
+        pauseOnHover: true,
+      }"
+      class="rounded-xl overflow-hidden shadow-xl"
+    >
+      <SplideSlide v-for="(img, i) in images" :key="i" class="">
+        <img :src="img" alt="" class="w-full h-auto object-cover" />
+      </SplideSlide>
+    </Splide>
+  </ClientOnly>
+</template>
+
+<script setup lang="ts">
+import { Splide, SplideSlide } from '@splidejs/vue-splide'
+import '@splidejs/splide/dist/css/splide.min.css'
+
+const images = [
+  '/images/screenshots/rasterflow1.png',
+  '/images/screenshots/rasterflow2.png',
+]
+</script>
