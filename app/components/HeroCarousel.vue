35bd300241fefc7d71470842ef1f54014798418b
Adam Emrts <adam.emrts@gmail.com>
2025-11-10 21:31:13 +0100

    🐛 feat(components): Use NuxtImg in HeroCarousel for optimized image loading

--- app/components/HeroCarousel.vue
+++ app/components/HeroCarousel.vue
@@ -19,10 +19,10 @@ const images = [
     :autoplay-interval="4000"
     class="w-full max-w-5xl mx-auto rounded shadow-lg overflow-hidden hover:scale-[1.1] hover:shadow-2xl hover:shadow-primary/20 bg-neutral-900 transition-all duration-500 flex items-center justify-center"
   >
-    <img
+    <NuxtImg
       :src="item"
       alt=""
       class="w-full h-auto object-contain"
-    >
+    />
   </UCarousel>
 </template>
