98c49485ecc3495e719c0721b04f0f71b72e87c3
Adam Emrts <adam.emrts@gmail.com>
2025-11-14 18:25:33 +0100

    ✨ feat(download): simplify download button component
    - remove automatic os detection
    - remove macOS download option
    - remove "all platforms" download option
    - use simple props to determine download url

--- app/components/DownloadButton.vue
+++ app/components/DownloadButton.vue
@@ -1,44 +1,24 @@
 <script setup lang="ts">
-import { ref, onMounted, computed } from 'vue'
-
 const props = defineProps<{
   version: string
-  system?: 'windows' | 'macos' | 'linux' | 'other'
+  system: 'windows' | 'linux'
 }>()
-
-const os = ref<'windows' | 'macos' | 'linux' | 'other'>(props.system ?? 'other')
 
 const baseUrl = 'https://github.com/flatscrew/rasterflow/releases/download'
 
-const urls = computed(() =>
-  ({
-    windows: `${baseUrl}/v${props.version}/RasterFlow-${props.version}-Setup-win64.exe`,
-    macos: `https://github.com/flatscrew/rasterflow/archive/refs/tags/v${props.version}.tar.gz`,
-    linux: `${baseUrl}/v${props.version}/RasterFlow-${props.version}.flatpak`
-  } as const)
-)
-
-const iconMap = {
-  windows: 'i-simple-icons-windows',
-  macos: 'i-simple-icons-apple',
-  linux: 'i-simple-icons-linux',
-  other: 'i-lucide-arrow-right'
+const urls = {
+  windows: `${baseUrl}/v${props.version}/RasterFlow-${props.version}-Setup-win64.exe`,
+  linux: `${baseUrl}/v${props.version}/RasterFlow-${props.version}.flatpak`
 } as const
 
-const labelMap = {
+const labels = {
   windows: 'Windows',
-  macos: 'macOS',
-  linux: 'Linux (Flatpak)',
-  other: 'All platforms'
+  linux: 'Linux (Flatpak)'
 } as const
 
-const downloadUrl = computed<string | undefined>(() => {
-  const pick = props.system ?? os.value
-  return pick === 'other' ? undefined : urls.value[pick]
-})
+const downloadUrl = urls[props.system]
 
-const displayLabel = computed(() => labelMap[props.system ?? os.value])
-const displayIcon = computed(() => iconMap[props.system ?? os.value])
+const displayLabel = labels[props.system]
 </script>
 
 <template>
@@ -46,7 +26,6 @@ const displayIcon = computed(() => iconMap[props.system ?? os.value])
     key="download"
     icon="i-lucide-download"
     size="xl"
-    :trailing-icon="displayIcon"
     :to="downloadUrl"
   >
     Download {{ version }} for {{ displayLabel }}
