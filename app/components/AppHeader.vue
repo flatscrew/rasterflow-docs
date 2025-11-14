dbaff395e6ee5ba800be8ba0a6f5eceeddce8ea6
Adam Emrts <adam.emrts@gmail.com>
2025-11-14 19:18:29 +0100

    ✨ feat(header): enhance mobile menu behavior
    - add mobileMenuProps computed property to control menu behavior based on route
    - conditionally set type and defaultOpen props for UContentNavigation component

--- app/components/AppHeader.vue
+++ app/components/AppHeader.vue
@@ -7,6 +7,16 @@ const { value: colorModeValue } = useColorMode()
 const desktopNavigation = computed(() => header.navigation.map(link => ({ ...link, active: route.path.startsWith(link.to) })))
 
 const navigation = inject<Ref<ContentNavigationItem[]>>('navigation')
+
+const mobileMenuProps = computed(() => route.path === '/'
+  ? {
+      type: 'single' as const,
+      defaultOpen: undefined
+    }
+  : {
+      type: undefined,
+      defaultOpen: true
+    })
 </script>
 
 <template>
@@ -74,8 +84,8 @@ const navigation = inject<Ref<ContentNavigationItem[]>>('navigation')
 
     <template #body>
       <UContentNavigation
+        v-bind="mobileMenuProps"
         highlight
-        :default-open="true"
         :navigation="navigation"
       />
     </template>
